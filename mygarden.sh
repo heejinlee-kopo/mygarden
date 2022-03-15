96b9b26970c3473e56930bf3615c2b44ebb8e8ca
HJ <heejinlnux@kopo>
2022-03-15 14:52:01 +0900

    periodical commit

--- mygarden.sh
+++ mygarden.sh
@@ -2,7 +2,7 @@
 cd /home/user1/git_test/mygarden
 git add .
 git commit -m "periodical commit"
-id=heejinlee-kopo
-passwd=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
-git push $id $passwd
+#id=heejinlee-kopo
+#passwd=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
+git push 
 exit 0
